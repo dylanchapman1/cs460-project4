212fb6637cba15fad13f1b3970923c3350ba2955
Andrew Hicks <andrewhicks920@gmail.com>
2025-04-25 13:58:38 -0700

    Added input handler

--- Prog4.java
+++ Prog4.java
@@ -10,19 +10,35 @@ import java.util.*;
 
 public class Prog4 {
     public static int getMenuChoice(Scanner scanner) {
-        System.out.println("Select a query");
-        System.out.println("1. Add, Update or Delete a Member");
-        System.out.println("2. Add Update, or Delete a Ski Pass");
-        System.out.println("3. Add, Update, or Delete an Equipment Inventory Record");
-        System.out.println("4. Add, Update, or Delete an Equipment Rental Record");
-        System.out.println("5. Add, Update, or Delete a Lesson Purchase Record");
+        while (true) {
+            System.out.println("""
+                Select a query:
+                Add, Update, or Delete a(n):
+                    Member (1), Ski Pass (2), Equipment Inventory Record (3),
+                    Equipment Rental Record (4), Lesson Purchase Record (5)
+                
+                6. Get Member Ski Lesson Details
+                7. Get Ski Pass Usage Details
+                8. Get Open Intermediate Trails
+                9. Query 4 (Custom Query)
+                10. Quit
+                """);
 
-        System.out.println("6. Get Member Ski Lesson Details");
-        System.out.println("7. Get Ski Pass Usage Details");
-        System.out.println("8. Get Open Intermediate Trails");
-        System.out.println("9. Query 4 (Custom Query)");
-        System.out.println("10. Quit");
-        return 0;
+            int choice;
+            try {
+                choice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (choice) {
+                    case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 -> {return choice;}
+                    default -> System.out.println("Please choose a number 1-10!");
+                }
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Please choose a number 1-5!");
+                scanner.nextLine();
+            }
+        }
     }
 
     public static void Member() {
@@ -70,7 +86,7 @@ public class Prog4 {
 
 
     public static void main(String[] args) {
-        final String oracleURL = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+        /*final String oracleURL = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
 
         String username = null; // Oracle DBMS username & password
         String password = null;
@@ -113,17 +129,15 @@ public class Prog4 {
 
 
         // Now that we've done the "pre-processing," let's get started with the queries!
-        // Queries will be built by these String objects (multiple times)
+        // Queries will be built by these String objects (multiple times) */
         Scanner scanner = new Scanner(System.in);
         Statement statement;
 
         try {
             int decision = getMenuChoice(scanner);
-            scanner.nextLine();
-            System.out.println();
 
             while (decision != 10) {
-                statement = dbconn.createStatement();
+                //statement = dbconn.createStatement();
                 switch (decision) {
                     case 1:
                         Member();
@@ -161,17 +175,18 @@ public class Prog4 {
                         CustomQuery();
                         break;
                 }
-                statement.close();
+                //statement.close();
                 decision = getMenuChoice(scanner);
             }
         }
-        catch (SQLException e) {
+        catch (Exception e) {System.out.println(e.getMessage());}
+        /*catch (SQLException e) {
             System.err.println("*** SQLException: Could not fetch query results.");
             System.err.println("\tMessage:   " + e.getMessage());
             System.err.println("\tSQLState:  " + e.getSQLState());
             System.err.println("\tErrorCode: " + e.getErrorCode());
             System.exit(-1);
-        }
+        } */
 
 
     }
